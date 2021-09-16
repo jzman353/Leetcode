48c85d78e9ddee15a9c127248dc92e31a0b81d14
jzman353 <jzman353@gmail.com>
2021-09-16 14:31:51 -0700

    more probs

--- Array/Spiral Matrix.py
+++ Array/Spiral Matrix.py
@@ -47,3 +47,60 @@ if __name__ == '__main__':
 
     assert test([[1,2,3],[4,5,6],[7,8,9]]) == [1,2,3,6,9,8,7,4,5]
     assert test([[1,2,3,4],[5,6,7,8],[9,10,11,12]]) == [1,2,3,4,8,12,11,10,9,5,6,7]
+
+"""
+64%
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        top = 1
+        bot = len(matrix)
+        left = 0
+        right = len(matrix[0])
+        matrix_flattened_size = len(matrix)*len(matrix[0])
+        
+        ans = []
+        pos = [0,0]
+        
+        def go_right(right):
+            while pos[0] < right-1 and len(ans)<matrix_flattened_size:
+                ans.append(matrix[pos[1]][pos[0]])
+                pos[0] += 1
+            right -= 1
+            #print(ans)
+        
+        def go_left(left):
+            while pos[0] > left and len(ans)<matrix_flattened_size:
+                ans.append(matrix[pos[1]][pos[0]])
+                pos[0] -= 1
+            left += 1
+            #print(ans)
+        
+        def go_down(bot):
+            while pos[1] < bot-1 and len(ans)<matrix_flattened_size:
+                ans.append(matrix[pos[1]][pos[0]])
+                pos[1] += 1
+            bot -= 1
+            #print(ans)
+            
+        def go_up(top):
+            while pos[1] > top and len(ans)<matrix_flattened_size:
+                ans.append(matrix[pos[1]][pos[0]])
+                pos[1] -= 1
+            top += 1
+            
+        if len(matrix[0]) == 1:
+            i=0
+            while len(ans)<matrix_flattened_size:
+                ans.append(matrix[i][0])
+                i += 1
+            return ans
+        
+        while len(ans) < matrix_flattened_size:
+                go_right(right)
+                go_down(bot)
+                go_left(left)
+                go_up(top)
+        
+        return ans
+"""
--- Other/NYtimes_Puzzle.py
+++ Other/NYtimes_Puzzle.py
@@ -3,17 +3,8 @@ words_file = open("../Needed_Modules/Word_List.txt", "r")
 english_words = words_file.read().splitlines()
 
 for i in english_words:
-    if len(i)==4 and i[-1] == 'b':
-        test = True
-        for j in ['o','u','r','s','g','f','m','j','p']:
-            if j in i:
-                test = False
-                break
-        if test:
-            print(i)
 
-"""
-bibb
-blab
-bleb
-"""
+    #if len(i)==5 and i[0] == 'e' and i[1] == 'e' and i[2] == 't' and i[3] == 'i' and i[4] == 't':
+    if len(i) == 5 and i[1] == 'm' and i[4] == 'e':
+        print(i)
+
